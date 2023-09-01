76a0dbf703adbd2c3253f562c9f0671bc171be12
Abhay Kesarwani <103197529+abhayKesarwa@users.noreply.github.com>
2023-09-01 15:53:32 +0530

    Add files via upload

---
+++ js/problem.js
@@ -0,0 +1,52 @@
+const openProblem1 = document.querySelector("#openProblem1");
+const openProblem2 = document.querySelector("#openProblem2");
+const openProblem3 = document.querySelector("#openProblem3");
+const openProblem4 = document.querySelector("#openProblem4");
+
+
+// const basicDialog = document.querySelector("#basicDialog");
+// const basicDialogModal = document.querySelector("#basicDialogModal");
+// const dialogWithBackdrop = document.querySelector("#dialogWithBackdrop");
+// const dialogWithAnimation = document.querySelector("#dialogWithAnimation");
+const output = document.querySelector("output");
+
+// "Show the dialog" button opens the <dialog> 
+openProblem1.addEventListener('click', () => {
+    Problem1.showModal();
+});
+
+openProblem2.addEventListener('click', () => {
+    Problem2.showModal();
+});
+
+openProblem3.addEventListener('click', () => {
+    Problem3.showModal();
+});
+
+openProblem4.addEventListener('click', () => {
+    Problem4.showModal();
+});
+
+
+
+
+
+
+// // "Show the dialog modally" button opens the <dialog> modally
+// openBasicDialogModal.addEventListener('click', () => {
+//     basicDialogModal.showModal();
+// });
+
+// // "Show the dialog with custom backdrop" button opens the <dialog> modally
+// openDialogWithBackdrop.addEventListener('click', () => {
+//     dialogWithBackdrop.showModal();
+// });
+
+// // "Show the dialog with animation" button opens the <dialog> modally
+// openDialogWithAnimation.addEventListener('click', () => {
+//     dialogWithAnimation.showModal();
+// });
+
+[basicDialog, dialogWithBackdrop, dialogWithAnimation].forEach(element => element.addEventListener("close", function () {
+    output.value = `✨ You clicked ${element.returnValue}`;
+}));
