b20fc15faecff35f3dde8d557641eb35af44c589
Rishabh Vaish <95993653+thehustler01@users.noreply.github.com>
2023-09-12 19:18:52 +0530

    Update clock.js

--- js/clock.js
+++ js/clock.js
@@ -13,7 +13,7 @@ dd = String(today.getDate()).padStart(2, "0"),
 mm = String(today.getMonth() + 1).padStart(2, "0"),
 yyyy = today.getFullYear(),
 nextYear = yyyy + 1,
-dayMonth = "09/14/",
+dayMonth = "09/24/",
 birthday = dayMonth + yyyy;
 
 today = mm + "/" + dd + "/" + yyyy;
